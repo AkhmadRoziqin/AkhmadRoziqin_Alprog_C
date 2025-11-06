cccc4132a5c630dcbd2adf4ddcaac862b809ebb5
AkhmadRoziqin <rzqn0513050a@gmail.com>
2025-11-06 16:06:10 +0800

    Add files via upload

---
+++ meet2.py
@@ -0,0 +1,24 @@
+# variabel adalah cara kita mendekatkan/mendeklarasikan nilai 
+print ("nama Akhmad Roaiqin")
+
+x = 3
+y = 13
+lebar = 80
+print ("nilai dari x =", x)
+print ("nilai dari y =", y)
+print ("nilai dari lebar =", lebar)
+
+# penamaan variabel
+#1 tidak boleh lebih dari satu kata, jika ada kata maka digabung/sisambung dengan underscor(_). = jumlah_z = 10
+#2 tidak boleh di awali angka. = (99bet = 388)x, (bet99 = 10000)oke
+#3 jika digabung dengan kata maka gunakan huruf kapital pada variabel. = jumlahZ = 10
+
+#pemanggilan indirect
+a = x
+print ("nilai dari a =", a)
+
+nama = "Akhmad Roziqin"
+nim = 25241071
+print ("nama saya =", nama)
+print ("nim saya =", nim
+)
