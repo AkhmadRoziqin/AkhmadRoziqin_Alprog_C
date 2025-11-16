d8497eb9588d9d44190f97c969c621da7b21a5dd
AkhmadRoziqin <rzqn0513050a@gmail.com>
2025-11-16 10:20:13 +0800

    Add user input for name and age with type display
    The program takes user input for name and age, displaying the values and their types.

---
+++ meet4.py
@@ -0,0 +1,37 @@
+# mengambil input dari user
+
+nama = input ("masukan nama anda ") 
+print("welcom back", nama)
+print("nilai dari nama", nama)
+print(type(nama))
+
+bil_int = int(input("masuk bilangan anda"))
+print("bilangan anda adalah", bil_int)
+print("nilai dari bil_int", bil_int)
+print(type(bil_int))
+
+bil_float = float(input("masuk bilangan anda"))
+print("bilangan anda adalah", bil_float)
+print("nilai dari bil_int", bil_float)
+print(type(bil_float))
+
+bil_bool = bool(input("masuk bilangan anda"))
+print("bilangan anda adalah", bil_bool)
+print("nilai dari bil_int", bil_bool)
+print(type(bil_bool))
+
+#buatlah program dengan output sebagi berikut
+# masuka nama anda:
+# masukan umur anda:
+# nama anda adalah (nama)
+# tipe data dari nama adalah (tipe dari nama)
+# umur anda adalah (umur)
+# tipe data dari umur anda adalah (tipe dari umur)
+
+nama = input ("masukan nama anda ") 
+print("nama saya adalah ", nama)
+print(type(nama))
+
+bil_int = int(input("masukan umur anda"))
+print("umur anda adalah", bil_int)
+print(type(bil_int))
