e1c0bf38b83ca03148a7e5119609e6fcf35e6154
AkhmadRoziqin <rzqn0513050a@gmail.com>
2025-11-30 13:37:49 +0800

    Add files via upload

---
+++ meet5.py
@@ -0,0 +1,40 @@
+#operasi aritmatika (x : - +)
+#penjumlahan
+a = 9
+b = 3
+hasil = a + b
+print(a, "+", b, "=", hasil) 
+
+#pengurangan 
+a = 10
+b = 5
+hasil = a-b
+print(a,"-",b,"=",hasil)
+#perkalian 
+a=13
+b=13
+hasil = a*b
+print(a,"x",b,"=",hasil)
+#pembagian
+a=45
+b=5
+hasil=a/b
+print(a,":",b,"=",hasil)
+
+#operasi prioritas (mana yang duluan dikerjakan dalam perhitungan matematika)
+ # 1. ()
+ # 2. pangkat
+ # 3. x :
+ # 4. + -
+
+ #perpangkatan
+hasil = a**2
+print(a,"pangkat2","=",hasil)
+#modulus (sisa dari pembagian)
+a = 13
+b = 2
+hasil =a%b
+print(a,"modulus",b,"=",hasil)
+#floor divison (pembulatan)
+hasil = a//b
+print(a,"floor div",b,"=",hasil)
