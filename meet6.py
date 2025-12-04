a7ba37fa9c5facf5173fe0e495585c5f9e778b30
AkhmadRoziqin <rzqn0513050a@gmail.com>
2025-12-04 17:48:59 +0800

    Add files via upload

---
+++ meet6.py
@@ -0,0 +1,10 @@
+# nama : Akhmad Roziqin
+# nim : 25241071
+# buatlah program konversi suhu(celcius, fahren, reamur dan kelvin)
+celcius = float(input("suhu dari celcius ="))
+fahren = 9/5*celcius+32
+print("suhu fahrenheit =", fahren)
+reamur = 4/5*celcius
+print("suhu dari reamur =", reamur) 
+kelvin = celcius+273.15
+print("suhu dari kelvin",kelvin)
