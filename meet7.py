1dd24cc7eccd6b03f255bb130d66f464b661c2d6
AkhmadRoziqin <rzqn0513050a@gmail.com>
2025-12-11 16:39:40 +0800

    Add files via upload

---
+++ meet7.py
@@ -0,0 +1,47 @@
+# Nama : Akhmad Roziqin
+# NIM  : 25241071
+
+#komparasi (true/false)
+# >, <, >=, <=, ==, !=, is is not
+x = 25
+y = 71
+print("========== lebih dari (>)") 
+hasil = x>5
+print(x,">",5,"=",hasil)
+hasil = x>50
+print(x,">",50,"=",hasil)
+print("========== kurang dari (<)")
+hasil = y<10
+print(y,"<",10,"=",hasil)
+hasil = y<100
+print(y,"<",100,"=",hasil)
+print("========== lebih besar dari sama dengan (>=)")
+hasil = y>=71
+print(y,">=",71,"=",hasil)
+hasil = y>=10
+print(y,">=",10,"=",hasil)
+print("========== lebih kecil dari sama dengan (<=)")
+hasil = x<=6
+print(x,"<=",6,"=",hasil)
+hasil = x<=25
+print(x,"<=",25,"=",hasil)
+print("========== sama dengan dari (==)")
+hasil = x==25
+print(x,"==",25,"=",hasil)
+hasil = x==90
+print(x,"==",90,"=",hasil)
+print("========== tidak sama dengan dari (!=)")
+hasil = x!=6
+print(x,"!=",6,"=",hasil)
+hasil = x!=25
+print(x,"!=",25,"=",hasil)
+print("========== sama dari (is)")
+hasil = y is 71
+print(y,"is",71,"=",hasil)
+hasil = y is 33
+print(y,"is",33,"=",hasil)
+print("========== tidak sama dari (is not)")
+hasil = x is not 5
+print(x,"is not",5,"=",hasil)
+hasil = x is not 25
+print(x,"is not",25,"=",hasil)
